616f80d6c0f3adb0adc1619e9b7dde30c254bc19
LeBaoPhuc <phucle1478@gmail.com>
2025-05-17 00:09:22 +0700

    new

--- Back_end/app.js
+++ Back_end/app.js
@@ -19,54 +19,9 @@ const employeeRouter = require("./routes/employee.route");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// const config = {
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   server: process.env.DB_SERVER,
-//   database: process.env.DB_NAME,
-//   options: {
-//     port: Number(process.env.DB_PORT), // Chuyển đổi sang số
-//     encrypt: true,
-//     trustServerCertificate: true,
-//   },
-// };
-
-// // Kết nối đến cơ sở dữ liệu
-// const connectToDatabase = async () => {
-//   try {
-//     await sql.connect(config);
-//     console.log("Kết nối thành công đến SQL Server");
-//   } catch (err) {
-//     console.error("Lỗi kết nối: ", err);
-//   }
-// };
-
-const mysql = require("mysql2");
-
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "kltn",
-  port: 3306,
-});
-
-const connectToDatabase = async () => {
-  connection.connect((err) => {
-    if (err) {
-      console.error("Kết nối thất bại: " + err.stack);
-      return;
-    }
-    console.log("Kết nối thành công với ID: " + connection.threadId);
-  });
-};
-
 // Middleware
 app.use(cors());
 app.use(express.json());
-
-// Kết nối đến cơ sở dữ liệu
-// connectToDatabase();
 
 // Endpoint cho bảng đơn hàng
 app.use(productRouter);
--- Back_end/db.js
+++ Back_end/db.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "kltn",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   port: 3306,
   waitForConnections: true,
   connectionLimit: 10,
